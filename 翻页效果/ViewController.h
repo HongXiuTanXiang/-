2ec1ddcf94cae0d974412961ba22f4f0de34dfab
lihe <lihe@promote.cache-dns.local>
2017-01-29 04:52:53 -0800

    Initial Commit

---
+++ 翻页效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  翻页效果
+//
+//  Created by lihe on 17/1/29.
+//  Copyright © 2017年 lihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 翻页效果/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  翻页效果
+//
+//  Created by lihe on 17/1/29.
+//  Copyright © 2017年 lihe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
